c14f98cc7af9cd39c2d44708e70d670e80315bf4
Gesare-n <114504698+Gesare-n@users.noreply.github.com>
2024-02-19 06:25:32 +0300

    Update Item.js

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
+// Functional component Item with props {name, category}
 function Item({ name, category }) {
+  // State variable to track whether the item is in the cart or not
+  const [isInCart, setIsInCart] = useState(false);
+
+  // Function to toggle the isInCart state
+  const handleToggleCart = () => {
+    // Toggle the value of isInCart
+    setIsInCart((prevIsInCart) => !prevIsInCart);
+  };
+
   return (
-    <li className="">
+    // Render an <li> element with conditional class based on isInCart state
+    <li className={isInCart ? "in-cart" : ""}>
+      {/* Display item name */}
       <span>{name}</span>
+      {/* Display item category */}
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      {/* Button to add/remove item from cart */}
+      <button className="add" onClick={handleToggleCart}>
+        {/* Conditional text based on isInCart state */}
+        {isInCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
